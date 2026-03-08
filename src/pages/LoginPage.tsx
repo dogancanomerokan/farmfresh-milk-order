12b6c42271dc86f266e1e324d4c97ef00f262ce9
Doğancan Ömer Okan <100826316+dogancanomerokan@users.noreply.github.com>
2026-03-08 19:31:55 +0300

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
-import OrderPage from "./OrderPage";
 import MemberPage from "./pages/MemberPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
