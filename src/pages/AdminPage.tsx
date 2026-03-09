a6cbae726fdfcbcc94699d1566af753310adfe3c
Doğancan Ömer Okan <100826316+dogancanomerokan@users.noreply.github.com>
2026-03-09 10:55:39 +0300

    Update AdminPage.tsx

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -730,9 +730,6 @@ const AdminPage = () => {
       </div>
     );
   }
-
-console.log("adminUsers", adminUsers);
-console.log("claimed_by_admin_id", order.claimed_by_admin_id);
   
   return (
     <div className="min-h-screen bg-background">
