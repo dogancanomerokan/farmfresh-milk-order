39892a98a2debc8da1c37a306790d35b41324d5f
Doğancan Ömer Okan <100826316+dogancanomerokan@users.noreply.github.com>
2026-05-07 11:58:55 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,14 +3,12 @@ import Hero from "@/components/Hero";
 import ProductShowcase from "@/components/ProductShowcase";
 import HowItWorks from "@/components/HowItWorks";
 import Footer from "@/components/Footer";
-import HomepageNoticeBanner from "@/components/HomepageNoticeBanner";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <Hero />
-      <HomepageNoticeBanner />
       <ProductShowcase />
       <HowItWorks />
       <Footer />
